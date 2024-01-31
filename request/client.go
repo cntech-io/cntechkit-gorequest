20f59039698e0fbb46859a0f4572525c532ef7a7
Yunus Kalkan <yunus@cntech.io>
2024-01-31 14:13:14 +0000

    added ByteArrayResult to client

--- request/client.go
+++ request/client.go
@@ -32,3 +32,23 @@ func (cl Client) Result(key string) (interface{}, error) {
 	}
 	return data, nil
 }
+
+func (cl Client) ByteArrayResult(key string) ([]byte, error) {
+	httpClient := &http.Client{}
+	req := cl[key]
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		fmt.Println("error making request for", key, "error: ", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	response, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("error reading response for", key, "error: ", err)
+		return nil, err
+	}
+    return response, err
+}
+
+
